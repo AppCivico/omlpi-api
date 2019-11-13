5d94a427fd1e42312a8697b734c4d968c4ba80a6
Junior Moraes <juniorfvox@gmail.com>
2019-11-13 15:10:51 -0300

    Concat state when object is a city on locales endpoint

--- lib/Andi/Model/Locale.pm
+++ lib/Andi/Model/Locale.pm
@@ -4,10 +4,28 @@ use Mojo::Base 'MojoX::Model';
 sub build_list {
     my $self = shift;
 
-    return $self->app->pg->db->select_p(
-        "locale",
-        [qw<id name type>],
-    );
+    return $self->app->pg->db->query_p(<<'SQL_QUERY');
+      SELECT
+        l.id,
+        CASE
+          WHEN l.type = 'city' THEN CONCAT(l.name, ', ', s.uf)
+          ELSE l.name
+        END AS name,
+        l.type
+      FROM locale l
+      LEFT JOIN city ct
+        ON l.type = 'city' AND l.id = ct.id
+      LEFT JOIN state s
+        ON l.type = 'city' AND ct.state_id = s.id
+      ORDER BY (
+        CASE l.type
+          WHEN 'country' THEN 1
+          WHEN 'state'   THEN 2
+          WHEN 'city'    THEN 3
+          WHEN 'region'  THEN 4
+        END
+      )
+SQL_QUERY
 }
 
 1;
