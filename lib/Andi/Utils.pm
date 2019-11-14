4863f965ca0d15a920a97614a6d73a97be406527
Junior Moraes <juniorfvox@gmail.com>
2019-11-14 10:14:17 -0200

    Add nullif helper

--- lib/Andi/Utils.pm
+++ lib/Andi/Utils.pm
@@ -4,13 +4,18 @@ use common::sense;
 use vars qw(@ISA @EXPORT);
 
 @ISA    = (qw(Exporter));
-@EXPORT = qw(is_test env);
+@EXPORT = qw(is_test env nullif);
 
 sub is_test {
     if ($ENV{HARNESS_ACTIVE} || $0 =~ m{forkprove}) {
         return 1;
     }
     return 0;
+}
+
+sub nullif {
+    my $value = shift;
+    return $value eq shift ? undef : $value;
 }
 
 sub env { return $ENV{${\shift}} }
