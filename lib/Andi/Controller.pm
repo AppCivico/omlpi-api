42c29f37f3f7f311d23bbf967721d075375ddb5c
Junior Moraes <juniorfvox@gmail.com>
2019-11-25 09:55:42 -0300

    Improve error handling

--- lib/Andi/Controller.pm
+++ lib/Andi/Controller.pm
@@ -4,15 +4,39 @@ use Mojo::Base 'Mojolicious::Controller';
 use Scalar::Util qw(blessed);
 
 sub reply_not_found {
-    die {error => 'page_not_found', message => 'Page not found', status => 404,};
+    die {
+        errors => [
+            {
+                message => "Page not found",
+                path    => undef,
+            }
+        ],
+        status => 404,
+    };
 }
 
 sub reply_forbidden {
-    die {error => 'permission_denied', message => 'Forbidden', status => 403,};
+    die {
+        errors => [
+            {
+                message => "Forbidden",
+                path    => undef,
+            }
+        ],
+        status => 403,
+    };
 }
 
 sub reply_method_not_allowed {
-    die {error => 'method_not_allowed', message => 'Method not allowed', status => 405,};
+    die {
+        errors => [
+            {
+                message => "Method not allowed",
+                path    => undef,
+            }
+        ],
+        status => 405,
+    };
 }
 
 sub reply_exception {
@@ -22,7 +46,15 @@ sub reply_exception {
     if ($@) {
         $c->app->log->fatal("reply_exception generated an exception!!!");
         $c->app->log->fatal($@);
-        die {error => 'internal_server_error', message => "Internal server error", status => 500};
+        die {
+            errors => [
+                {
+                    message => "Internal server error",
+                    path    => undef,
+                }
+            ],
+            status => 500,
+        };
     }
     return $ret if $ret;
 }
@@ -33,10 +65,10 @@ sub _reply_exception {
 
     if ($an_error) {
 
-        if (ref $an_error eq 'HASH' && exists $an_error->{error}) {
-            my $status = delete $an_error->{status} || 400;
+        if (ref $an_error eq 'HASH' && ref $an_error->{errors} eq 'ARRAY') {
+            my $status = $an_error->{status} || 400;
 
-            return $c->render(json => $an_error, status => $status,);
+            return $c->render(json => $an_error, status => $status);
         }
         elsif (ref $an_error eq 'REF' && ref $$an_error eq 'ARRAY' && @$$an_error == 2) {
 
@@ -89,56 +121,6 @@ sub _reply_exception {
     }
 
     return $c->render(json => {error => 'internal_server_error', message => "Internal server error"}, status => 500);
-}
-
-sub validate_request_params {
-    my ($c, %fields) = @_;
-
-    foreach my $key (keys %fields) {
-        my $me   = $fields{$key};
-        my $type = $me->{type};
-
-        my $val = $c->req->params->to_hash->{$key};
-        $val = '' if !defined $val && $me->{clean_undef};
-
-        if (defined($val) && (exists $me->{min_length} || exists $me->{max_length})) {
-            my $len     = length $val;
-            my $min_len = $me->{min_length};
-            my $max_len = $me->{max_length};
-            die {error => 'form_error', message => {$key => 'invalid'}, status => 400}
-              if (defined($min_len) && $len < $min_len) || (defined($max_len) && $len > $max_len);
-        }
-
-        if (!defined $val && $me->{required} && !($me->{undef_is_valid} && !defined $val)) {
-            die {error => 'form_error', message => {$key => 'missing'}, status => 400,};
-        }
-
-        if (
-               defined $val
-            && $val eq ''
-            && (   $me->{empty_is_invalid}
-                || $type eq 'Bool'
-                || $type eq 'Int'
-                || $type eq 'Num'
-                || ref $type eq 'MooseX::Types::TypeDecorator')
-          )
-        {
-
-            die {error => 'form_error', message => {$key => 'empty_is_invalid'}, status => 400,};
-        }
-
-        next unless $val;
-
-        my $cons = Moose::Util::TypeConstraints::find_or_parse_type_constraint($type);
-
-        if (!defined $cons) {
-            die {error => 'form_error', message => "Unknown type constraint '$type'", status => 400,};
-        }
-
-        if (!$cons->check($val)) {
-            die {error => 'form_error', message => {$key => 'invalid'}, status => 400,};
-        }
-    }
 }
 
 1;
