c8c577760773f94e552135fb6367c5b4ecd91d25
Junior Moraes <juniorfvox@gmail.com>
2019-11-25 09:59:24 -0300

    Improve internal server error exception

--- lib/Andi/Controller.pm
+++ lib/Andi/Controller.pm
@@ -39,6 +39,18 @@ sub reply_method_not_allowed {
     };
 }
 
+sub reply_internal_server_error {
+    die {
+        errors => [
+            {
+                message => "Internal server error",
+                path    => undef,
+            }
+        ],
+        status => 500,
+    };
+}
+
 sub reply_exception {
     my $c   = shift;
     my $err = shift;
@@ -46,15 +58,7 @@ sub reply_exception {
     if ($@) {
         $c->app->log->fatal("reply_exception generated an exception!!!");
         $c->app->log->fatal($@);
-        die {
-            errors => [
-                {
-                    message => "Internal server error",
-                    path    => undef,
-                }
-            ],
-            status => 500,
-        };
+        return $c->reply_internal_server_error();
     }
     return $ret if $ret;
 }
@@ -70,57 +74,12 @@ sub _reply_exception {
 
             return $c->render(json => $an_error, status => $status);
         }
-        elsif (ref $an_error eq 'REF' && ref $$an_error eq 'ARRAY' && @$$an_error == 2) {
-
-            return $c->render(
-                json   => {error => 'form_error', message => {$$an_error->[0] => $$an_error->[1]}},
-                status => 400,
-            );
-        }
-        elsif (ref $an_error eq 'DBIx::Class::Exception'
-            && $an_error->{msg} =~ /duplicate key value violates unique constraint/)
-        {
-            $c->app->log->info('Exception treated: ' . $an_error->{msg});
-
-            return $c->render(
-                json => {
-                    error   => 'duplicate_key_violation',
-                    message => 'You violated an unique constraint! Please verify your input fields and try again.'
-                },
-                status => 400,
-            );
-        }
-        elsif (ref $an_error eq 'DBIx::Class::Exception' && $an_error->{msg} =~ /is not present/) {
-            my ($match, $value) = $an_error->{msg} =~ /Key \((.+?)\)=(\(.+?)\)/;
-
-            return $c->render(
-                json => {
-                    error   => 'fk_violation',
-                    message => sprintf 'key=%s value=%s cannot be found on our database',
-                    $match, $value
-                },
-                status => 400,
-            );
-        }
-        elsif (ref $an_error eq 'HASH' && $an_error->{error_code}) {
-            $c->app->log->info('Exception treated: ' . $an_error->{message});
-
-            return $c->render(
-                json   => {error => 'generic_exception', message => $an_error->{message}},
-                status => $an_error->{error_code} || 500,
-            );
-        }
-        elsif (ref $an_error eq 'HASH' && $an_error->{error}) {
-            $c->app->log->info('Exception treated: ' . $c->app->dumper($an_error));
-
-            return $c->render(json => {error => delete $an_error->{error}, %$an_error}, status => 400,);
-        }
 
         $c->app->log->fatal(blessed($an_error)
               && UNIVERSAL::can($an_error, 'message') ? $an_error->message : $c->app->dumper($an_error));
     }
 
-    return $c->render(json => {error => 'internal_server_error', message => "Internal server error"}, status => 500);
+    return $c->reply_internal_server_error();
 }
 
 1;
