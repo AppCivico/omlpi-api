24f13cbad2091209ed0704610afda196b57cad3a
Junior Moraes <juniorfvox@gmail.com>
2019-11-28 19:44:30 -0200

    Annonate a TODO

--- lib/Andi/Model/Locale.pm
+++ lib/Andi/Model/Locale.pm
@@ -36,6 +36,7 @@ sub get {
     my @locale_ids = ref $opts{locale_id} eq 'ARRAY' ? @{$opts{locale_id}} : ($opts{locale_id});
     my @binds     = ((($year) x 8), (($area_id) x 2), @locale_ids);
 
+    # TODO No indicator_values e subindicator values, passar o WHERE do locale_id
     return $self->app->pg->db->query(<<"SQL_QUERY", @binds);
       SELECT
         locale.id   AS id,
