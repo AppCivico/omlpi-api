ca844e633cfc2b2c71c9378e73723489ff85e44e
Junior Moraes <juniorfvox@gmail.com>
2019-12-06 15:42:35 -0200

    Add download data controller

---
+++ lib/Andi/Controller/Data/Download.pm
@@ -0,0 +1,20 @@
+package Andi::Controller::Data::Download;
+use Mojo::Base 'Andi::Controller';
+
+use Data::Printer;
+
+sub get {
+    my $c = shift;
+
+    $c->render_later();
+    return $c->model('Data')->get_all_data()
+      ->then(sub {
+          p \@_;
+          ...
+      })
+      ->catch(sub {
+          return $c->reply_exception(@_);
+      });
+}
+
+1;
