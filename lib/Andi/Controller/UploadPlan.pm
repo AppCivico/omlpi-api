421b26dda7f1a6e9e3cba5e1effb1f2da3974866
Junior Moraes <juniorfvox@gmail.com>
2020-01-07 16:46:40 -0200

    Create email object with file as attachment

--- lib/Andi/Controller/UploadPlan.pm
+++ lib/Andi/Controller/UploadPlan.pm
@@ -2,17 +2,52 @@ package Andi::Controller::UploadPlan;
 use Mojo::Base 'Andi::Controller';
 
 use DDP;
+use Data::Section::Simple qw(get_data_section);
+use Andi::Minion::Task::SendEmail::Mailer::Template;
+use File::Temp;
 
 sub post {
     my $c = shift;
 
     $c->openapi->valid_input() or return;
 
-    my $file = $c->param('file');
-    p $file;
+    # File upload
+    my $upload = $c->param('file');
+    my $fh = File::Temp->new(UNLINK => 1);
+    $upload->move_to($fh);
 
+    my $email = Andi::Minion::Task::SendEmail::Mailer::Template->new(
+        to       => 'carlos@appcivico.com',
+        from     => 'no-reply@appcivico.com',
+        subject  => 'Upload de plano municipal',
+        template => get_data_section('template.tt'),
+        vars     => {
+            name  => $c->param('name'),
+            email => $c->param('email'),
+        },
+        attachments => [
+            {
+                fh   => $fh,
+                name => $upload->filename,
+            }
+        ]
+    )->build_email();
+
+    $c->minion->enqueue(send_email => [ $email->as_string() ]);
+
+    close $fh;
 
     return $c->render(json => {}, status => 200);
 }
 
 1;
+
+__DATA__
+
+@@ template.tt
+
+Nome: [%name%]
+<br>
+Email: [%email%]
+<br>
+Cidade: [%city%]
--- t/integration/012-upload-plan.t
+++ t/integration/012-upload-plan.t
@@ -11,9 +11,17 @@ subtest_buffered 'UploadPlan | post' => sub {
 
     my $city = $pg->db->select('city', ['id'], undef, { limit => 1 } )->hash;
 
-    $t->post_ok("/v1/upload_plan", form => { name => 'Junior M', file => { file => "$RealBin/../data/logo.pdf" }, city_id => $city->{id}, email => 'foobar@appcivico.com' })
+    $t->post_ok("/v1/upload_plan", form => {
+        name    => 'Junior M',
+        city_id => $city->{id},
+        email   => 'carlos@appcivico.com',
+        file    => { file => "$RealBin/../data/logo.pdf" },
+      })
       ->status_is(200);
+
     p $t->tx->res->json;
+
+    ok $t->app->minion->perform_jobs();
 };
 
 done_testing();
