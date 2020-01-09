5819433b0b64d1effaa0d7b2cbca4ed78aa0a55d
Junior Moraes <juniorfvox@gmail.com>
2020-01-09 15:32:33 -0300

    Zip file to send file as email attachment

--- lib/OMLPI/Controller/UploadPlan.pm
+++ lib/OMLPI/Controller/UploadPlan.pm
@@ -4,6 +4,8 @@ use Mojo::Base 'OMLPI::Controller';
 use File::Temp;
 use Mojo::Util qw(decode);
 use OMLPI::Utils qw(mojo_home);
+#use IO::Compress::Zip qw(zip $ZipError);
+use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
 use OMLPI::Minion::Task::SendEmail::Mailer::Template;
 
 use DDP;
@@ -18,9 +20,18 @@ sub post {
     my $fh = File::Temp->new(UNLINK => 1);
     $upload->move_to($fh);
 
+    my $zip = Archive::Zip->new();
+    my $filename = $upload->filename;
+    $zip->addFile($fh->filename, $filename);
+
+    my $zipfile = File::Temp->new(UNLINK => 0, SUFFIX => ".zip");
+    $zip->writeToFileNamed($zipfile->filename);
+
+    close $fh;
+
     # Locale
-    my $locale_id = $c->param('locale_id');
-    my $locale = $c->model('Locale')->get_state_or_city_name_with_uf($locale_id)->hash;
+    my $locale_id   = $c->param('locale_id');
+    my $locale      = $c->model('Locale')->get_state_or_city_name_with_uf($locale_id)->hash;
     my $locale_type = $locale->{type};
     if (!$locale_type =~ m{^(city|state)$}) {
         return $c->render(
@@ -44,6 +55,8 @@ sub post {
     # Fix encoding issues
     my $slurp = decode('UTF-8', $template->slurp);
 
+    $filename =~ s/(.*)\.[^.]+$/$1.zip/g;
+
     my $email = OMLPI::Minion::Task::SendEmail::Mailer::Template->new(
         to       => 'carlos@appcivico.com',
         from     => 'no-reply@appcivico.com',
@@ -56,15 +69,16 @@ sub post {
         },
         attachments => [
             {
-                fh   => $fh,
-                name => $upload->filename,
+                fh   => $zipfile,
+                name => $filename,
             }
         ]
     )->build_email();
 
     $c->minion->enqueue(send_email => [ $email->as_string() ]);
 
-    close $fh;
+    close $zipfile;
+    unlink $zipfile->filename or die $!;
 
     return $c->render(json => {}, status => 200);
 }
