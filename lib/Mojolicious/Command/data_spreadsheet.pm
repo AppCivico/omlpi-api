89949f0227c317d6fdd7b63bf39ee6c13232bb54
Junior Moraes <juniorfvox@gmail.com>
2020-03-25 13:06:51 -0300

    Add command to generate spreadsheet with all data

---
+++ lib/Mojolicious/Command/data_spreadsheet.pm
@@ -0,0 +1,25 @@
+package Mojolicious::Command::data_spreadsheet;
+use Mojo::Base 'Mojolicious::Command';
+
+use File::Copy;
+
+has description => 'Generate static data spreadsheet';
+
+sub run {
+    my $self = shift;
+
+    my $app = $self->app;
+    $app->log->info("Generating file...");
+    $app->model('Data')->get_all_data()
+      ->then(sub {
+        my $file = shift;
+
+        my $home = $app->home;
+        move($file->filename, $home->rel_file('public/data_spreadsheet.xlsx')) or die $!;
+
+        $app->log->info("File generated!");
+  });
+  Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
+}
+
+1;
