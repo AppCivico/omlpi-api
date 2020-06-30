2fa6d37f99917d351fe6bea9f5e9f46372228deb
Junior Moraes <juniorfvox@gmail.com>
2020-06-30 19:16:02 -0300

    feat: aggregate keeping the subindicator order on historical endpoint.

--- lib/OMLPI/Controller/Data/Historical.pm
+++ lib/OMLPI/Controller/Data/Historical.pm
@@ -1,6 +1,8 @@
 package OMLPI::Controller::Data::Historical;
 use Mojo::Base 'OMLPI::Controller';
 
+use Tie::IxHash;
+
 sub get {
     my $c = shift;
 
@@ -9,9 +11,34 @@ sub get {
     my $area_id = $c->param('area_id');
     my $locale_id = $c->param('locale_id');
 
-    my $res = $c->model('Historical')->get_historical(locale_id => $locale_id, area_id => $area_id);
+    my $res = $c->model('Historical')
+      ->get_historical(locale_id => $locale_id, area_id => $area_id)
+      ->expand
+      ->hashes;
 
-    return $c->render(json => { historical => $res->expand->hashes }, status => 200 );
+    # Aggregate the subindicators by the classification
+    my $fake_classification_id = 1;
+    for my $locale (@{$res}) {
+        for my $indicator (@{$locale->{indicators} || []}) {
+            my @subindicators = sort { $a->{id} <=> $b->{id} } @{ delete $indicator->{subindicators} };
+
+            my %agg;
+            tie(%agg, 'Tie::IxHash');
+            for my $subindicator (@subindicators) {
+                my $classification = delete $subindicator->{classification};
+                $agg{$classification} //= {
+                    id             => $fake_classification_id++,
+                    data           => [],
+                    classification => $classification,
+                };
+                push @{ $agg{$classification}->{data} }, $subindicator;
+            }
+
+            $indicator->{subindicators} = [values %agg];
+        }
+    }
+
+    return $c->render(json => { historical => $res }, status => 200 );
 }
 
 1;
